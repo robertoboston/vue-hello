1f9fa564f5f0504b36b0b89ee1651f5f2c7ee9ac
roberto.boston <roberto.bozzo97@tiscali.it>
2022-12-16 14:42:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const { createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            message : 'hello vue js ti amo',
+            image : 'IMG_1987.JPG'
+        }
+    }
+}).mount('#hello-vue')
